1c692b204c0e714f0827187b08c3668ddeb5cbd4
M00NR000M <84885574+M00NR000M@users.noreply.github.com>
2021-05-28 10:27:58 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <historys></historys>
+  <contacts></contacts>
+</template>
+
+<script>
+import Historys from "./page/Historys.vue";
+import Contacts from "./page/Contacts.vue";
+export default {
+  name: "App",
+  components: {
+    Historys: Historys,
+    Contacts: Contacts,
+  },
+};
+</script>
---
+++ src/page/Historys.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul v-for="history in historys" :key="history.id">
+    <li>
+      <button>
+        {{ history.id }}
+        {{ history.Fname }}
+        {{ history.Lname }}
+        {{ history.Day }}
+        {{ history.Old }}
+        {{ history.Title }}
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    historys: [
+      { id: "Profile" },
+      { id: "ชื่อ", Fname: "พิรยะ" },
+      { id: "นามสกุล", Lname: "ยอดแก้ว" },
+      { id: "วันเกิด", Day: "2/05/2000" },
+      { id: "อายุ", Old: "21" },
+      { id: "ทักษะ" },
+      { Title: "C#,C++,Java,SQL,HTML" },
+    ],
+  }),
+};
+</script>
